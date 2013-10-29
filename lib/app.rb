bd212b4082d8656b7a4b609d59fadfa89b748620
Rolen Le <rolentle@gmail.com>
2013-10-28 18:05:55 -0600

    Add Index.erb

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
+require 'better_errors'
 
 class CloneWarsApp < Sinatra::Base
+  set :root, 'lib/app'
+  configure :development do
+    use BetterErrors::Middleware
+    BetterErrors.application_root = "lib/app"
+  end
+
   get '/' do
+    erb :index
   end
 end
